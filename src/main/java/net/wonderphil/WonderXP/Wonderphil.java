7a31c470e7edd98590f9bd625a8ac4209ff72226
Philip Xu <jianmx@microsoft.com>
2021-03-17 00:19:30 -0700

    Initial commit of WonderXP

---
+++ src/main/java/net/wonderphil/WonderXP/Wonderphil.java
@@ -0,0 +1,11 @@
+package net.wonderphil.WonderXP;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Wonderphil implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
